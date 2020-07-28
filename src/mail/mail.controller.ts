96fd9cbb26f8203fa32e12d78abcdb3437acdda0
kk <705555262@qq.com>
2020-07-28 10:01:24 +0800

    feat(mail) 添加mail的控制器

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { ApiOperation } from '@nestjs/swagger';
+
+@Controller('mail')
+export class MailController {
+  constructor(private readonly mailService: MailService) {
+  }
+
+  @Get("send")
+  @ApiOperation({summary: '发送邮件'})
+  sendEmail() {
+    this.mailService.send("705555262@qq.com", "测试邮件")
+  }
+}
