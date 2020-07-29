9cfcc71bcc6867e602f1334c2f157f285d546b02
kk <705555262@qq.com>
2020-07-29 14:56:08 +0800

    refactor(test) register简单测试

--- src/users/users.e2e-spec.ts
+++ src/users/users.e2e-spec.ts
@@ -10,6 +10,7 @@ import { AuthService } from '../auth/auth.service';
 import { LocalStrategy } from '../auth/local.strategy';
 import { JwtStrategy } from '../auth/jwt.strategy';
 import { getModelToken } from '@nestjs/mongoose';
+import { RegisterUserDto } from './dtos/register-user.dto';
 
 describe('Users Controller', () => {
   let controller: UsersController;
@@ -42,4 +43,21 @@ describe('Users Controller', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
   });
+
+  describe('register', () => {
+    it('should 提示用户名已存在', async () => {
+      const result = {
+        code: 400,
+        msg: '用户已存在'
+      }
+      // @ts-ignore
+      const registerUserDto: RegisterUserDto = {
+        username: 'liyuankun',
+        password: 'liyuankun',
+        name: 'kk'
+      }
+      jest.spyOn(service, 'register').mockImplementation(async (): Promise<any> => Promise.resolve(result))
+      expect(await controller.register(registerUserDto)).toBe(result)
+    });
+  })
 });
